ea700ebc4758fbfe2c74efd1e342fa34695f0a71
Niilo <then1ppe@gmail.com>
2018-04-20 01:02:00 +0300

    Terrain painting and quest updates

---
+++ Assets/Scriptit/DestroyObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour {
+
+    public GameObject QuestScript;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (QuestScript.GetComponent<quest>().startedQuest == true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
